1624d425e3eddbeea4fe43c55b91a7c4dd3afff0
marti19686 <marti19686@gmail.com>
2017-11-09 21:07:24 +0100

    Create class Address

---
+++ Isengard.App/Address.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Isengard.App
+{
+    class Address : IInformations
+    {
+        public string Street { get; private set; }
+        public string City { get; private set; }
+        public string PostCode { get; private set; }
+        public string StreetNumber { get; private set; }
+
+        public void ShowDetails()
+        {
+            
+        }
+
+        public void SetData()
+        {
+            
+        }
+    }
+    
+}

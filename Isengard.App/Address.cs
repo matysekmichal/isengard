b8257b7b72f9568ae0f82c4bde530a148c45428e
marti19686 <marti19686@gmail.com>
2017-11-14 21:13:24 +0100

    Stworzenie klasy Toolbox, dodanie konstruktorów i metods do klasy Address

--- Isengard.App/Address.cs
+++ Isengard.App/Address.cs
@@ -11,21 +11,41 @@ namespace Isengard.App
         public string Street { get; private set; }
         public string City { get; private set; }
         public string PostCode { get; private set; }
-        public string StreetNumber { get; private set; }
+        public int StreetNumber { get; private set; }
 
         public Address()
         {
-            
+            this.SetData(Toolbox.NoData, Toolbox.NoData, Toolbox.NoData, Toolbox.NoNumber);
         }
 
+        public Address(string Street, string City, string PostCode, int StreetNumber) : this()
+        {
+            this.SetData(Street,City,PostCode,StreetNumber);
+        }
         public void ShowDetails()
         {
             
         }
 
-        public void SetData()
+        public static Address SetData() //Podananie informacji dla wszystkich pól i przekazanie do metody SetData
         {
-            
+            Address result = new Address();
+
+            Console.WriteLine("Wprowadź dane nowego adresu");
+
+            result.SetData(Toolbox.inputString("Podaj ulicę: ",false), 
+                          Toolbox.inputString("Podaj Miasto: ", false),
+                          Toolbox.inputString("Podaj Kod Pocztowy: ", false), 
+                          Toolbox.inputInteger("Nr Domu: ",1,200));
+            return result;
+        }
+
+        public void SetData(string Street, string City, string PostCode, int StreetNumber)
+        {
+            this.Street = Street;
+            this.City = City;
+            this.PostCode = PostCode;
+            this.StreetNumber = StreetNumber;
         }
     }
     
---
+++ Isengard.App/Toolbox.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Isengard.App
+{
+    public class Toolbox
+    {
+        public static string NoData = "Brak Danych";
+        public static int NoNumber = 0;
+
+        public static int inputInteger(string inputText, int min, int max)
+        {
+            int result = 0;
+
+            while (true)
+            {
+                string liczba = Toolbox.inputString(inputText, false);
+
+                result = int.Parse(liczba);
+
+                if (!int.TryParse(liczba, out result) ||
+                    (result < min || result > max))
+                {
+                    Console.WriteLine("Wprowadzono niepoprawna wartosc (Zakres {0} {1})", min, max);
+                }
+                else
+                    break;
+            }
+
+            return result;
+        }
+
+        public static string inputString(string inputText, bool isEmpty)
+        {
+            string result = "";
+
+            while (true)
+            {
+                Console.Write(inputText);
+                result = Console.ReadLine();
+
+                if (isEmpty)
+                {
+                    break;
+                }
+                else
+                if (string.IsNullOrEmpty(result))
+                {
+                    Console.WriteLine("Wartosc nie moze byc pusta!");
+                }
+                else
+                    break;
+            }
+
+            return result;
+        }
+    }
+
+}
